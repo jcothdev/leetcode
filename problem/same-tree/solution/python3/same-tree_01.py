4ec1f2f690180c12a3ebf53f8cf5ae0dd4838c58
jcothdev <jonathan@coth.dev>
2025-02-01 11:59:46 -0500

    same-tree cleanup refactor

--- problem/same-tree/solution/python3/same-tree_01.py
+++ problem/same-tree/solution/python3/same-tree_01.py
@@ -7,10 +7,7 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         isSame = False
-        if p.val == q.val and (p.left and q.left):
-            if p.left.val == q.left.val:
-                print("QQ")
-            if (p.left and q.left) and (p.right.val == q.right.val):
-                print("QQQ")
+        if p.val == q.val and (p.left and q.left) and (p.left.val == q.left.val):
+            if (p.right and q.right) and (p.right.val == q.right.val):
                 isSame = True
         return isSame
