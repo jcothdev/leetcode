be37d2523227a67ab567721cc83149d99eb7e429
jcothdev <jonathan@coth.dev>
2025-02-07 09:58:21 -0500

    merge-two-sorted-lists 01 + git housekeeping

--- problem/same-tree/solution/python3/same-tree_01.py
+++ problem/same-tree/solution/python3/same-tree_01.py
@@ -7,7 +7,12 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         isSame = False
-        if p.val == q.val and (p.left and q.left) and (p.left.val == q.left.val):
-            if (p.right and q.right) and (p.right.val == q.right.val):
-                isSame = True
+        if (
+            p.val == q.val
+            and (p.left and q.left)
+            and (p.left.val == q.left.val)
+            and (p.right and q.right)
+            and (p.right.val == q.right.val)
+        ):
+            isSame = True
         return isSame
